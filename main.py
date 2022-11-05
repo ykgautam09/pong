32f16623d6d5f62b4149146678fa0bd0fb5b5d58
ykgautam09 <yateeshgautam1357@gmail.com>
2022-11-06 04:18:43 +0530

    backup

--- main.py
+++ main.py
@@ -33,11 +33,29 @@ class Ball(pygame.sprite.Sprite):
                            (self.radius, self.radius), self.radius)
 
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y, size=(10, 50), colour=(255, 255, 255)) -> None:
+        super().__init__()
+        self.pos_x = x
+        self.pos_y = y
+        self.size = size
+        self.colour = colour
+        self.image = pygame.Surface(self.size)
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        pygame.draw.rect(self.image, self.colour, (200,50,120,25))
+
+
 # create game
 ball = Ball(150, 200, 1)
 ball_group = pygame.sprite.Group()
 ball_group.add(ball)
 
+player1 = Player(10, WINDOW_HEIGHT//2)
+player2 = Player(WINDOW_WIDTH-20, WINDOW_HEIGHT//2)
+player_group = pygame.sprite.Group()
+player_group.add(player1)
+player_group.add(player2)
 
 run = True
 while run:
@@ -54,5 +72,7 @@ while run:
         # agents action
         ball_group.draw(screen)
         ball_group.update()
+        player_group.draw(screen)
+        player_group.update()
     pygame.display.update()
     clock.tick(FPS)  # control fps
